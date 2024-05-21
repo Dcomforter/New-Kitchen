3e847f1a1ddf76b1c0fef115697ca8f3bb2ad454
Israel Okuneye <104809559+Dcomforter@users.noreply.github.com>
2024-05-20 20:27:35 -0500

    Update settings.py

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'secondproject.wsgi.application'
 
 
 Database
-https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
