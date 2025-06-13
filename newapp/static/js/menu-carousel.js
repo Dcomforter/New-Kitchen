b03d6ea457bae5272a8add6dde285da9fde7ef55
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-06-13 17:35:52 -0500

    Added menu carousel and Swiper class

---
+++ newapp/static/js/menu-carousel.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Swiper('.menu-carousel', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      }
+    }
+  });
+});
