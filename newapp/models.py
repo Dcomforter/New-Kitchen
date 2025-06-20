1b261795aecf58f715c7ab9047c23d727e6b39f9
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-06-19 22:01:31 -0500

    Updated models.py with image field

--- newapp/models.py
+++ newapp/models.py
@@ -32,7 +32,7 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     prep_time = models.IntegerField(default=25)
     calories = models.IntegerField(default=1000)
-    #image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
+    image = models.ImageField(upload_to='menu_images/', null=True, blank=True)
     
     def __str__(self):
         return (
