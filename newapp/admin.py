0493a9e583374a137896876c8a0f5754d5d052ee
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-06-20 16:35:17 -0500

    Updated admin.py and kitchen.html

--- newapp/admin.py
+++ newapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from newapp.models import Booking, Menu
 
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('food_name', 'cuisine', 'price')
+    fields = ('food_name', 'cuisine', 'item_description', 'price', 'image')  # Include image here
+
+
 # Register your models here.
 admin.site.register(Booking)
 admin.site.register(Menu)
