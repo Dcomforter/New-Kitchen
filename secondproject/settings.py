722e2cb156abb78c2116778c7c4fb5829df3c243
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-06-21 08:12:29 -0500

    Updated settings.py and added new image to database

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6epzh+g=xc=i45)r_iavwsow9hqeyea_b3n)ew3mp+07=pkt7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
