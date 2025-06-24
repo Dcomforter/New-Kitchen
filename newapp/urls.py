dce36a6050059e42e91f9e319738f8b327f9f76b
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-06-24 14:02:58 -0500

    Updated urls.py with a route that includes item_id

--- newapp/urls.py
+++ newapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('kitchen/', views.kitchen, name='kitchen'),
     path('reservation/', views.form_view, name='reservation' ),
-    path('menu_details/<int:pk>', views.menu_details, name='menu_details'),
+    path('menu_details/<int:item_id>', views.menu_details, name='menu_details'),
     path('kitchen/menu_details/<int:pk>', views.menu_details, name='menu_details'), 
     path('about/', views.about, name="about"),   
 ]
