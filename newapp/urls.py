b19995c3568dfab5f1dedbbd2648efcbb0e7110b
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-06-24 16:42:21 -0500

    Added a new route for order_success in urls.py

--- newapp/urls.py
+++ newapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('menu_details/<int:item_id>/', views.menu_details, name='menu_details'),
     path('kitchen/menu_details/<int:item_id>/', views.menu_details, name='menu_details'),
     path('place_order/<int:item_id>/', views.place_order, name='place_order'), 
+    path('order_success/', views.order_success, name='order_success'),
     path('about/', views.about, name="about"),   
 ]
 
