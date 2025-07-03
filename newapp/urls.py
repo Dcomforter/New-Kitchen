0dbf7555778077575f55aa9a121ddd26cb0b3e73
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-07-03 17:09:33 -0500

    Updated urls.py

--- newapp/urls.py
+++ newapp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('cart/', views.cart_view, name='cart_view'),
     path('cart/add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout, name='checkout'),
     path('submit_order/', views.submit_order, name='submit_order'),       
 ]
 
