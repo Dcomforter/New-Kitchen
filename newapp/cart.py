2ad7252887a2596e2ba926ae270411d675fe9a03
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2025-07-03 15:56:41 -0500

    Updated cart.py

--- newapp/cart.py
+++ newapp/cart.py
@@ -5,3 +5,17 @@ class Cart:
         if not cart:
             cart = self.session['cart'] = {}
         self.cart = cart
+
+    def add(self, menu_item_id, quantity=1):
+        menu_item_id = str(menu_item_id)
+        if menu_item_id in self.cart:
+            self.cart[menu_item_id] += quantity
+        else:
+            self.cart[menu_item_id] = quantity
+        self.save()
+
+    def remove(self, menu_item_id):
+        menu_item_id = str(menu_item_id)
+        if menu_item_id in self.cart:
+            del self.cart[menu_item_id]
+            self.save()    
